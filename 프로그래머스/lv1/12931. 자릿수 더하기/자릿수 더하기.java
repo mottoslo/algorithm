0f7c7bb5174371eec55142da7359021142b900c7
mottoslo <c4save@korea.ac.kr>
2023-04-08 21:28:28 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.72 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String num = String.format("%d",n);
+        
+        for (int i = 0 ; i< num.length();i++){
+            answer += Integer.parseInt(num.substring(i,i+1));
+        }
+        return answer;
+    }
+}
