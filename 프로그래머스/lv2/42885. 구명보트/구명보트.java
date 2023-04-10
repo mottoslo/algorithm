fd5f9cabb127dbe47c395a0b850f46a39d2cf7a2
강재형 <c4save@korea.ac.kr>
2023-04-10 15:46:05 +0900

    [level 2] Title: 구명보트, Time: 9.99 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,26 +2,23 @@ import java.util.*;
 class Solution {
     public static int solution(int[] people, int limit) {
 
-        List<Integer> pplList = new ArrayList<Integer>();
-        for (int ppl:people){
-            pplList.add(ppl);
-        }
+        Arrays.sort(people);
         int idx_L = 0;
-        int idx_R = pplList.size()- 1;
+        int idx_R = people.length- 1;
         int cnt = 0;
-        Collections.sort(pplList, Collections.reverseOrder());
 
         while(idx_L <= idx_R){
-            int Lperson = pplList.get(idx_L);
-            int Rperson = pplList.get(idx_R);
+            int Lperson = people[idx_L];
+            int Rperson = people[idx_R];
             if (idx_L == idx_R){
                 cnt ++;
                 break;
             }
             if (Lperson + Rperson <= limit){
-                idx_R --;
+                idx_L++;    
             }
-            idx_L++;
+            
+            idx_R --;
             cnt++;
             }
         return cnt;
