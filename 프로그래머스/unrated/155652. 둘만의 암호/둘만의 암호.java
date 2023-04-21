1d2cc70743dc4fdd194eec16cea67fd443ce44fe
강재형 <c4save@korea.ac.kr>
2023-04-21 21:34:41 +0900

    [unrated] Title: 둘만의 암호, Time: 12.43 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -2,16 +2,13 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        List<Character> skipchar = new ArrayList<>();
-        for(int i = 0; i<skip.length(); i++){
-            skipchar.add(skip.charAt(i));
-        }
         for(int i = 0; i<s.length();i++){
             char nowchar = s.charAt(i);
             int idx = 0;
             while(idx != index){
-                nowchar = nowchar+1<=122 ? (char)(nowchar + 1) : (char)(nowchar - 25);
-                if(!skipchar.contains(nowchar)){
+                nowchar = (char) (nowchar+1);
+                nowchar = nowchar<=122 ? nowchar : (char)(nowchar - 26);
+                if(!skip.contains(String.valueOf(nowchar))){
                     idx++;
                 }
             }
