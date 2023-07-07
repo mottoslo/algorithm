b2521ce10ddaf93ed34760c2eb6135c49b11aace
강재형 <c4save@korea.ac.kr>
2023-07-08 01:39:22 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,28 +1,35 @@
-from collections import Counter, defaultdict, deque
-import heapq
+from collections import defaultdict 
 
-def bfs(que, dic, tickets, target):
-    answers = []
-    while(len(que) > 0):
-        usedTickets, now = que.pop()
-        if len(usedTickets) == target:
-            answers.append([destination for depart, destination in usedTickets])
-        else:
-            for destination in dic[now]:
-                if usedTickets.count((now, destination)) < dic[now].count(destination):
-                    que.append((usedTickets + [(now, destination)], destination))
-    
-    answers.sort()
-    return answers[0]
+def dfs(graph, N, key, footprint):
+    print(footprint)
+
+    if len(footprint) == N + 1:
+        return footprint
+
+    for idx, country in enumerate(graph[key]):
+        graph[key].pop(idx)
+
+        tmp = footprint[:]
+        tmp.append(country)
+
+        ret = dfs(graph, N, country, tmp)
+
+        graph[key].insert(idx, country)
+
+        if ret:
+            return ret
+
+
 def solution(tickets):
-    answer = 0
-    target = len(tickets)
-    dic = defaultdict(list)
+    answer = []
+
+    graph = defaultdict(list)
+
+    N = len(tickets)
     for ticket in tickets:
-        dic[ticket[0]].append(ticket[1])
-    que = deque()
-    for i in range(len(dic["ICN"])):
-        start = dic["ICN"][i]
-        que.append(([("ICN", start)], start))
-    answer = bfs(que, dic, tickets, target)
-    return ["ICN"] + answer
+        graph[ticket[0]].append(ticket[1])
+        graph[ticket[0]].sort()
+
+    answer = dfs(graph, N, "ICN", ["ICN"])
+
+    return answer
