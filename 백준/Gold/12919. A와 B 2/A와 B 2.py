a8367ee5a70c0c682e8a76a30e5761f5c8ddf912
강재형 <c4save@korea.ac.kr>
2023-07-16 11:05:37 +0900

    [Gold V] Title: A와 B 2, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -2,17 +2,21 @@ import sys
 
 S=list(input())
 T=list(input())
+ans = 0
 
-def dfs(t):
-    if t==S:
-        print(1)
-        sys.exit()
+def dfs(t,s):
+    global ans
+    if t==s:
+        ans += 1
     if len(t)==0:
         return 0
     if t[-1]=='A':
-        dfs(t[:-1])
+        dfs(t[:-1], s)
     if t[0]=='B':
-        dfs(t[1:][::-1])
+        dfs(t[1:][::-1], s)
 
-dfs(T)
-print(0)
+dfs(T,S)
+if ans > 0:
+    print(1)
+else:
+    print(0)
